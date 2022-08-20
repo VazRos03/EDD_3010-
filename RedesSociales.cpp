3ee8fac43fda589b1a92be759458c274bbe18ae3
VazRos03 <luisyuyots@gmail.com>
2022-08-20 13:43:12 -0500

    Estructura de datos

---
+++ RedesSociales.cpp
@@ -0,0 +1,35 @@
+/*Vázquez Rosales Luis Arturo
+Ingenieria en computacion
+422051561
+Facultad de Estudios Superiores Aragón*/
+
+#include<iostream>
+
+
+using namespace std;
+
+int main(){
+	//meses twitter
+	int ene = 62404, jun= 67496;
+	
+	cout<<"\tPRESENCIA REDES SOCIALES";
+	
+	int res = jun - ene;
+	
+	cout<<"\n TWITTER";
+	
+	cout<<"\nDiferencia de seguidores entre elmes de Enero y Junio: "<<res<<" seguidores";
+	
+	cout<<"\n YOUTUBE \n";
+	
+	//meses youtube visualizaciones
+	
+	int meses[6]={12226,15605,19602,19452,21805,17344};
+	
+	cout<<meses[2];
+	
+	
+	
+	
+	return 0;
+}
