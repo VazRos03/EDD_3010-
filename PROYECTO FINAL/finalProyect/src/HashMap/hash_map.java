c0d1a33efd5f49d012c7150bc0a8cd8b83ae40be
VazRos03 <luisyuyots@gmail.com>
2022-12-10 19:32:40 -0600

    Proyect

--- PROYECTO FINAL/finalProyect/src/HashMap/hash_map.java
+++ PROYECTO FINAL/finalProyect/src/HashMap/hash_map.java
@@ -4,10 +4,42 @@
  */
 package HashMap;
 
+//importando biblioteca para el uso de hasHMap
+import java.util.HashMap;
+
 /**
  *
  * @author Arturo
  */
 public class hash_map {
-    
+    public static void main(String[] args) {
+        
+        //declarando a la clase HashMap con un objeto llamado "diccionario"
+        HashMap diccionario = new HashMap();
+        
+        //estableciendo valores en el objeoto diccionario
+        diccionario.put("user", "Arturo");
+        diccionario.put("age", "18");
+        diccionario.put("height", "1.75");
+        
+        //accediendo a las key del diccionario
+         String nombre = diccionario.get("user").toString();
+         System.out.println(nombre);
+         
+         String edad = diccionario.get("age").toString();
+         System.out.println(edad);
+         
+         String altura = diccionario.get("height").toString();
+         System.out.println(altura);
+         
+         //comprobando que existe una key
+         boolean existe = diccionario.containsKey("marital status");
+         System.out.println(existe);
+         
+         System.out.println("---------eliminando elementos----------");
+         //quitando elemento del diccionario
+         boolean adios = diccionario.remove("height", "1.75");
+         System.out.println(adios);
+         System.out.println(diccionario);
+    }
 }
